9bacab12e3cf7a45610854e109ce052063217490
Hassan Amin <142036202+blurpp69@users.noreply.github.com>
2023-10-17 12:05:13 +0800

    update: flex

--- app/page.js
+++ app/page.js
@@ -2,18 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <p className="break-all">
-        jfkjndkjfndjfdsjknfknfks ddsjknfknfks ddsjknfknfks ddsjknfknfks
-        ddsjknfknfksddsjknfknfks ddsjknfknfks ddsjknfknfks dsjfjnsdknfkf kndf
-        kdsj
-      </p>
-      {/* <p className="text-3xl ">Title 1</p>
-      <p className="text-xl"> Title 2</p>
-      <p className="text-xl"> Title 3</p>
-      <p className="text-base uppercase">a regular paragraph</p>
-      <p className="text-sm"> A description paragraph </p>
-      <p className="text-xs note">A little note</p> */}
+    <div className="Parent">
+      <div>Header</div>
+      <div className="flex items-baseline ...">
+        <div className="pt-2 pb-6 bg-sky-500">01</div>
+        <div className="pt-8 pb-12 bg-red-500">02</div>
+        <div className="pt-12 pb-4 bg-green-500">03</div>
+      </div>
     </div>
   );
 }
